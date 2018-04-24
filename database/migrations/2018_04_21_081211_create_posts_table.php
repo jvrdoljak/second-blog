73db10f8287b432cad405ba0b6f5f1f1dd746e26
jvrdoljak <vrdoljakjosip@outlook.com>
2018-04-24 19:52:32 +0200

    All migrations are created. But relations isn't completed

--- database/migrations/2018_04_21_081211_create_posts_table.php
+++ database/migrations/2018_04_21_081211_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 50);
-            $table->string('email', 100)->unique();
-            $table->string('password', 16);
-            $table->rememberToken();
+            $table->string('title', 80);
+            $table->text('body');
+            $table->integer('created_by'); // created by user (id)
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
