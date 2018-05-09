4414641885288d239c255dcecf8852a0fd50d5f1
jvrdoljak <vrdoljakjosip@outlook.com>
2018-05-09 10:19:18 +0200

    .

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,17 @@
+@extends('main')
+
+@section('title', '| Edit post')
+
+@section('content')
+
+<div class="row mt-2">
+    <div class="col-md-2">
+        <a href="#" class="btn btn-success btn-block">Save changes</a>
+    </div>
+    <div class="col-md-2">
+        <a href="#" class="btn btn-danger btn-block">Cancel</a>
+    </div>
+</div>
+<hr>
+
+@endsection
