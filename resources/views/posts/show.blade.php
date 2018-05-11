b3bbe8a81c7e91e139742fa6ac017e51f645a39c
jvrdoljak <vrdoljakjosip@outlook.com>
2018-05-11 10:24:27 +0200

    Completed post managing

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,13 @@
 @section('title', '| Show posts')
 
 @section('content')
+    <div class="row mt-2">
+        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-secondary mr-2">Edit</a>
+        {!! Form::open(array('route' => array('posts.destroy', $post->id), 'method' => 'DELETE'))  !!}
+            {{ Form::submit('Delete', array('class' => 'form-control btn btn-danger')) }}
+        {!! Form::close() !!}
+    </div>
+
 
     <h1>{{ $post->title }}</h1>
     <hr>
